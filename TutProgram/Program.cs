7d7be73b4434cae044924be78ed218fcb0fcc758
Alfred Johnsson <04ALJOC@skola.lidkoping.se>
2021-12-15 13:30:47 +0100

    Add project files.

---
+++ TutProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace TutProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AntalISträng('o', "orvar käkar kooooorvar"));
+        }
+        static int AntalISträng(char tecken, string text)
+        {
+            int antalggr = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (tecken == text[i])
+                {
+                    antalggr++;
+                }
+            }
+            return antalggr;
+        }
+    }
+}
